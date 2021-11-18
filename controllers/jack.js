2cad5b0e937c6e1a8bf5ea28169e6285b779389b
Asiddhartha <siddhartha.reddy95@gmail.com>
2021-11-17 22:50:41 -0600

    updated

--- controllers/jack.js
+++ controllers/jack.js
@@ -104,3 +104,17 @@ exports.jack_view_one_Page = async function(req, res) {
         res.send(`{'error': '${err}'}`); 
     } 
 }; 
+
+// Handle building the view for creating a jack. 
+// No body, no in path parameter, no query. 
+// Does not need to be async 
+exports.jack_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('jackcreate', { title: 'jack Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+};
--- routes/jack.js
+++ routes/jack.js
@@ -7,4 +7,6 @@ router.get('/', jack_controllers.jack_view_all_Page );
 router.get('/jack/:id', jack_controllers.jack_view_all_Page);
 /* GET detail jack page */ 
 router.get('/detail', jack_controllers.jack_view_one_Page); 
+/* GET create jack page */ 
+router.get('/create', jack_controllers.jack_create_Page); 
 module.exports = router; 
