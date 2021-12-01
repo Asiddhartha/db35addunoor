1880911ee8c3e31d37ec4c9210122854a2dc3dc6
Asiddhartha <siddhartha.reddy95@gmail.com>
2021-12-01 15:12:37 -0600

    updated the files

--- routes/jack.js
+++ routes/jack.js
@@ -4,13 +4,13 @@ var router = express.Router();
 
 // A little function to check if we have an authorized user and continue on 
 // or  redirect to login. 
-const secured = (req, res, next) => { 
-    if (req.user){ 
-      return next(); 
-    } 
-    req.session.returnTo = req.originalUrl; 
-    res.redirect("/login"); 
-  } 
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+}
 
 ///* GET home page. */
 //router.get('/', function (req, res, next) {
@@ -20,13 +20,13 @@ const secured = (req, res, next) => {
 /* GET jacks */
 router.get('/', jack_controllers.jack_view_all_Page);
 /* GET detail jack page */
-router.get('/detail', jack_controllers.jack_view_one_Page);
+router.get('/detail', secured, jack_controllers.jack_view_one_Page);
 /* GET create jack page */
-router.get('/create', jack_controllers.jack_create_Page);
-/* GET update costume page */ 
-router.get('/update', jack_controllers.jack_update_Page);
+router.get('/create', secured, jack_controllers.jack_create_Page);
+/* GET update costume page */
+router.get('/update', secured, jack_controllers.jack_update_Page);
 /* GET delete jack page */
-router.get('/delete', jack_controllers.jack_delete_Page); 
+router.get('/delete', secured, jack_controllers.jack_delete_Page);
 /* GET details with id of jack page */
 router.get('/jack/:id', jack_controllers.jack_detail);
 /* GET delete with id of jack page */
